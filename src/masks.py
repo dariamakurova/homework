93ede59253ebc73fe711bb8a4ac46b734210464d
Daria_Makurova <dariamakurova@gmail.com>
2025-07-30 23:14:50 +0300

    fixing first homework stage

---
+++ src/masks.py
@@ -0,0 +1,18 @@
+def get_mask_card_number(card_number: int) -> str:
+    """преобразование номера карты клиента в маску вида XXXX XX** **** XXXX"""
+
+    if len(str(card_number)) == 16 and str(card_number).isdigit():
+        card_mask = str(card_number)[:4] + " " + str(card_number)[4:6] + "** **** " + str(card_number)[12:16]
+        return card_mask
+    else:
+        return "Номер должен состоять из 16 цифр"
+
+
+def get_mask_account(account: int) -> str:
+    """преобразование номера счета в маску вида **XXXX"""
+
+    if len(str(account)) == 20 and str(account).isdigit():
+        account_mask = "**" + str(account)[-4:]
+        return account_mask
+    else:
+        return "Номер должен состоять из 20 цифр"
