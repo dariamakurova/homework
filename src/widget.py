764642a028277625db824b4894340779e3b1cba9
Daria_Makurova <dariamakurova@gmail.com>
2025-07-31 00:28:58 +0300

    corrects in widget with linters

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
 from masks import get_mask_card_number, get_mask_account
 
+
 def mask_account_card(card_info: str) -> str:
     """Функция, которая маскирует номер карты или счета в полученной строке с информауией"""
     card_number = None
@@ -9,17 +10,21 @@ def mask_account_card(card_info: str) -> str:
     for item in card_info:
         if item.isdigit():
             undefined_number.append(item)
-        else: comment.append(item)
+        else:
+            comment.append(item)
 
     if len(undefined_number) == 16:
-         card_number = int("".join(undefined_number))
-         return "".join(comment) + get_mask_card_number(card_number)
+        card_number = int("".join(undefined_number))
+        return "".join(comment) + get_mask_card_number(card_number)
 
     elif len(undefined_number) == 20:
-         account_number = int("".join(undefined_number))
-         return "".join(comment) + get_mask_account(account_number)
+        account_number = int("".join(undefined_number))
+        return "".join(comment) + get_mask_account(account_number)
+
+    else:
+        return "Проверьте правильность ввода"
 
 
-if __name__ == "__main__":
-    card_info = "Счет 73654108430135874305"
-    print (mask_account_card(card_info))
+# if __name__ == "__main__":
+#     card_info = "Счет 73654108430135874305"
+#     print(mask_account_card(card_info))
