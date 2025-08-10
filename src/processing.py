411f9c6ef10e18b1dd388253be35cec546ab15bc
Daria_Makurova <dariamakurova@gmail.com>
2025-08-11 00:05:58 +0300

    creating filter_by_state in processing

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,8 @@
-def filter_by_state():
-    pass
+def filter_by_state(list_of_dicts: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Функция, которая фильтрует список словарей по заданному значению ключа state"""
+    new_list = []
+    for item in list_of_dicts:
+        if item["state"] == state:
+            new_list.append(item)
+    return new_list
 
