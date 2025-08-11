c0287e20a8ffd1d9b856c62d01357c28818b8f25
Daria_Makurova <dariamakurova@gmail.com>
2025-08-11 21:47:02 +0300

    correcting main.py with flake8 & mypy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.masks import get_mask_card_number, get_mask_account
 
 if __name__ == "__main__":
-  print(get_mask_card_number(1234567891234567))
-  print(get_mask_card_number(91234567891234567))
+    print(get_mask_card_number(1234567891234567))
+    print(get_mask_card_number(91234567891234567))
 
-  print(get_mask_account(1234567891234567890))
+    print(get_mask_account(1234567891234567890))
