8c4f08bdd3428c939ac79687536e5c147bf25e82
Daria_Makurova <dariamakurova@gmail.com>
2025-08-11 11:22:40 +0300

    correcting processing with flake8 & mypy

--- src/processing.py
+++ src/processing.py
@@ -9,6 +9,5 @@ def filter_by_state(list_of_dicts: list[dict], state: str = "EXECUTED") -> list[
 
 def sort_by_date(list_of_dicts: list[dict], is_reverse: bool = True) -> list[dict]:
     """Функция, которая сортирует список словарей по дате"""
-    sorted_list = sorted(list_of_dicts, key = lambda item: item["date"], reverse = is_reverse)
+    sorted_list = sorted(list_of_dicts, key=lambda item: item["date"], reverse=is_reverse)
     return sorted_list
-
