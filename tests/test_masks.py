228d2a21354feb82d0043cc063a3723d45c9bca6
Daria_Makurova <dariamakurova@gmail.com>
2025-08-15 00:18:11 +0300

    asserts in test_masks, fixtures for masks in conftest

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,15 +1,18 @@
+import pytest
+
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def test_get_mask_card_number():
-    assert get_mask_card_number(1234567890001234) == "1234 56** **** 1234"
-    assert get_mask_card_number(9123 - 4567 - 8912 - 4567) == "Номер должен состоять только из цифр без пробелов"
-    assert get_mask_card_number(12345678900012347) == "Номер должен состоять из 16 цифр"
-    assert get_mask_card_number(()) == "Номер карты не указан"
+def test_get_mask_card_number(card_number_right, card_number_with_symbols, card_number_long, card_number_empty):
+    assert get_mask_card_number(card_number_right) == "1234 56** **** 1234"
+    assert get_mask_card_number(card_number_with_symbols) == "Номер должен состоять только из цифр без пробелов"
+    assert get_mask_card_number(card_number_long) == "Номер должен состоять из 16 цифр"
+    assert get_mask_card_number(card_number_empty) == "Номер карты не указан"
 
 
-def test_get_mask_account_number():
-    assert get_mask_account(12345678999987654321) == "**4321"
-    assert get_mask_account(12345 - 67899 - 99876 - 54321) == "Номер должен состоять только из цифр без пробелов"
-    assert get_mask_account(1234567899998765432100) == "Номер должен состоять из 20 цифр"
-    assert get_mask_account(()) == "Номер счета не указан"
+def test_get_mask_account_number(account_right, account_with_symbols, account_long, account_empty):
+    assert get_mask_account(account_right) == "**4321"
+    assert get_mask_account(account_with_symbols) == "Номер должен состоять только из цифр без пробелов"
+    assert get_mask_account(account_long) == "Номер должен состоять из 20 цифр"
+    assert get_mask_account(account_empty) == "Номер счета не указан"
