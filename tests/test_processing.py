6621c2384bfa9a1c58f663bd14473c945e5578f5
Daria_Makurova <dariamakurova@gmail.com>
2025-08-18 23:39:31 +0300

    test_sort_by_date with mark.parametrize

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,6 +1,7 @@
 import pytest
 
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
+
 
 @pytest.mark.parametrize("session_info, state, result", [
     ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
@@ -33,4 +34,27 @@ from src.processing import filter_by_state
 def test_filter_by_state(session_info, state, result):
     assert filter_by_state(session_info, state) == result
 
+
+@pytest.mark.parametrize("session_info, result", [
+    (
+    [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
+    [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
+    (
+    [{'id': 41428829, 'state': 'EXECUTED', 'date': ''},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': ''},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
+    [{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 41428829, 'state': 'EXECUTED', 'date': ''},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': ''}]
+     )])
+def test_sort_by_date(session_info, result):
+    assert sort_by_date(session_info) == result
 
