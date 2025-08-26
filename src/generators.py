422dfa017aac4607c6629d1f38e3a76c3e45c11a
Daria_Makurova <dariamakurova@gmail.com>
2025-08-26 22:43:33 +0300

    final changes + pytest + coverage

--- src/generators.py
+++ src/generators.py
@@ -3,8 +3,7 @@ from typing import Any, Iterator
 
 def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[Any]:
     """Функция возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной"""
-    return filter(lambda x: x.get("operationAmount", {}).get("currency", {}).get("name", {})  == currency, transactions)
-
+    return filter(lambda x: x.get("operationAmount", {}).get("currency", {}).get("name", {}) == currency, transactions)
 
 
 def transaction_descriptions(transactions: list[dict]) -> Iterator[Any]:
@@ -22,47 +21,3 @@ def card_number_generator(start: int, stop: int) -> Iterator[Any]:
             yield final_numb
     else:
         yield "Допустимый диапазон номеров карт: 0 - 9999999999999999"
-
-
-if __name__ == "__main__":
-
-    transactions = [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод со счета на счет",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702",
-        },
-        {
-            "id": 939719571,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702",
-        },
-        {
-            "id": 939719572,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {"amount": "9824.07", "currency": {"name": "EUR", "code": "EUR"}},
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702",
-        },
-    ]
-
-    usd_transactions = filter_by_currency(transactions, "RUB")
-    print(next(usd_transactions, "Операций не найдено"))
-    print(next(usd_transactions, "Операций не найдено"))
-
-    descriptions = transaction_descriptions(transactions)
-    for _ in range(3):
-        print(next(descriptions))
-
-    for card_number in card_number_generator(1, 5):
-        print(card_number)
