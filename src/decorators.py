d992f82dd7e022740107bb29f4caa53bf6b94723
Daria_Makurova <dariamakurova@gmail.com>
2025-08-31 23:24:12 +0300

    decorators.py corrected version

--- src/decorators.py
+++ src/decorators.py
@@ -7,23 +7,23 @@ def log(filename):
             try:
                 func(*args, **kwargs)
                 if filename:
-                    with open(filename, "a", encoding="utf-8") as file:
+                    with open(filename, "w", encoding="utf-8") as file:
                         file.write(f'{func.__name__} OK\n')
                 else:
                     print(f'{func.__name__} OK')
             except Exception as e:
                 if filename:
-                    with open(filename, "a", encoding="utf-8") as file:
+                    with open(filename, "w", encoding="utf-8") as file:
                         file.write(f'{func.__name__} error: {e}. Inputs: {args}, {kwargs}')
                 else:
                     print(f'{func.__name__} error: {e}. Inputs: {args}, {kwargs}')
         return wrapper
     return decorator
 
-if __name__ == "__main__":
-    @log(filename="mylog.txt")
-    def my_function(x, y):
-        return x + y
-
-
-    my_function("f", 2)
+# if __name__ == "__main__":
+#     @log(filename="mylog.txt")
+#     def my_function(x, y):
+#         return x + y
+#
+#
+#     my_function("f", 2)
