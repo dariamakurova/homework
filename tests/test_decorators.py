b0c78be63ce1d3aa92a6febce8318648dbd7bb16
Daria_Makurova <dariamakurova@gmail.com>
2025-08-31 23:48:11 +0300

    test_decorators tests with no filename

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -32,3 +32,33 @@ def test_log_with_filename_error(): #тестирование декоратор
     with open("mylog.txt", "r", encoding="utf-8") as file:
         text = file.read()
         assert text == 'divide error: division by zero. Inputs: (5, 0), {}'
+
+
+def test_log_nofilename_ok(capsys): #тестирование декоратора с выводом в консоль и работающей функцией
+    @log(filename="")
+    def add(x, y):
+        return x + y
+
+    add(1, 2)
+    out = capsys.readouterr().out
+    assert out == "add OK\n"
+
+
+def test_log_nofilename_ok(capsys): #тестирование декоратора с выводом в консоль и TypeError
+    @log(filename="")
+    def add(x, y):
+        return x + y
+
+    add("f", 2)
+    out = capsys.readouterr().out
+    assert out == 'add error: can only concatenate str (not "int") to str. Inputs: (\'f\', 2), {}\n'
+
+
+def test_log_with_filename_error(capsys): #тестирование декоратора с выводом в консоль и ZeroDivisionError
+    @log(filename="")
+    def divide(x, y):
+        return x / y
+
+    divide(5, 0)
+    out = capsys.readouterr().out
+    assert out == 'divide error: division by zero. Inputs: (5, 0), {}\n'
