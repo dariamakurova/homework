df165d2156c3e2c34acbb0039f2e2f86090fabf5
Daria_Makurova <dariamakurova@gmail.com>
2025-09-05 23:56:13 +0300

    get_financial_operations() adde to utils

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+import json
+
+def get_financial_operations(path: str) -> list[dict]:
+    """ Получение списка словарей с финансовыми операциями из json файла"""
+
+    try:
+        with open(path) as f:
+            try:
+                financial_operations = json.load(f)
+            except json.JSONDecodeError:
+                return []
+    except FileNotFoundError:
+        return []
+    return financial_operations
