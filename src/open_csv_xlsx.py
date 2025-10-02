51f64e5ede2a5998de0fe83dbe119408b7e34317
Daria_Makurova <dariamakurova@gmail.com>
2025-10-02 22:11:05 +0300

    linters applied

--- src/open_csv_xlsx.py
+++ src/open_csv_xlsx.py
@@ -1,17 +1,19 @@
-import pandas as pd
 import os
+from typing import Optional
+
+import pandas as pd
 
 
-def get_transactions_from_csv(path: str, file_delimiter: str =';') -> list:
+def get_transactions_from_csv(path: str, file_delimiter: str = ";") -> Optional[list]:
     """Получение списка транзакций из .csv файла"""
     if os.path.exists(path):
         try:
-            transactions = pd.read_csv(path, delimiter=file_delimiter).to_dict(orient='records')
+            transactions = pd.read_csv(path, delimiter=file_delimiter).to_dict(orient="records")
         except pd.errors.ParserError:
-            print(f'Ошибка чтения CSV. Проверьте разделитель или формат данных.')
+            print("Ошибка чтения файла. Проверьте разделитель или формат данных.")
             return []
     else:
-        print(f'Файл {path} не найден')
+        print(f"Файл {path} не найден")
         return []
     return transactions
 
@@ -20,10 +22,10 @@ def get_transactions_from_excel(path: str) -> list:
     """Получение списка транзакций из .xlsx файла"""
     if os.path.exists(path):
         try:
-            transactions = pd.read_excel(path).to_dict(orient='records')
+            transactions = pd.read_excel(path).to_dict(orient="records")
         except ValueError:
             return []
     else:
-        print(f'Файл {path} не найден')
+        print(f"Файл {path} не найден")
         return []
     return transactions
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
+
 import pytest
-import os
 
 
 # фикстуры для номеров карт
