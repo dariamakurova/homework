480399cc542f07016af5c5c0490eac282bb5044d
Daria_Makurova <dariamakurova@gmail.com>
2025-10-18 02:59:46 +0300

    process_bank_operations added to processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,9 @@
 import re
+from collections import Counter, defaultdict
+
+from mypy.checkexpr import defaultdict
+
+from src.open_csv_xlsx import get_transactions_from_csv
 
 
 def filter_by_state(list_of_dicts: list[dict], state: str = "EXECUTED") -> list[dict]:
@@ -19,9 +24,25 @@ def sort_by_date(list_of_dicts: list[dict], is_reverse: bool = True) -> list[dic
 def process_bank_search(data: list[dict], search: str) -> list[dict]:
     """Функция, которая принимает список словарей с данными о банковских операциях и строку поиска,
     а возвращает список словарей, у которых в описании есть данная строка"""
-    # pattern = re.compile(search)
     return [
         element
         for element in data
         if isinstance(element.get("description"), str) and re.search(search, element["description"], re.IGNORECASE)
     ]
+
+def process_bank_operations(data:list[dict], categories:list)->dict:
+    """Функцию, которая принимает список словарей с данными о банковских операциях и список категорий операций,
+    а возвращает словарь, в котором ключи — это названия категорий,
+    а значения — это количество операций в каждой категории."""
+
+    matched_operations = Counter([element["description"] for element in data if isinstance(element.get("description"), str)
+                          and element["description"] in categories])
+    result = {}
+    for category, amount in matched_operations.items():
+        result[category] = amount
+    return result
+
+if __name__ == "__main__":
+    data = get_transactions_from_csv('/Users/dariamakurova/PycharmProjects/Homework/data/transactions.csv')
+    categories = ['Перевод организации', 'Открытие вклада', 'Неизвестная операция']
+    print(process_bank_operations(data, categories))
